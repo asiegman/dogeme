44595d18d6baa384423b8c2a590fb4d1cefa4750
Rowan Manning <accounts@rowanmanning.co.uk>
2013-11-28 18:52:57 +0000

    wow. so unduplicate. such sorting

--- dogeme.js
+++ dogeme.js
@@ -12,10 +12,13 @@ module.exports = {
         'http://24.media.tumblr.com/6a723af35ae1f123aa55182a60e5c181/tumblr_muzpkylW9o1szim6vo1_400.png',
         'http://24.media.tumblr.com/8a99c81b0242e1381fd540ff5782cacd/tumblr_mv84huyuN11s4bl2qo1_400.gif',
         'http://24.media.tumblr.com/a49a8730304a219ec067b38b2a077342/tumblr_muqdtiQmCk1szim6vo1_400.gif',
+        'http://24.media.tumblr.com/cce2287cd5eb22ad85d416f33cdfc4c2/tumblr_mqasxzAD5z1szim6vo1_400.jpg',
         'http://25.media.tumblr.com/8d5cd50a3f82387f9c06a3061f6e9792/tumblr_mwjcw2H8TE1r65re8o1_400.jpg',
         'http://25.media.tumblr.com/ed19cb78e4f950d5ab0e1891ce5d31e6/tumblr_mrxz4wOBV51szim6vo1_400.jpg',
+        'http://2damnfunny.com/wp-content/uploads/2013/10/Shibe-Doge-Declares-Siberia-The-Home-Of-Wow.jpg',
         'http://31.media.tumblr.com/06382138c587c0fdc62e9ecf6cfb9697/tumblr_mrl5dxZHyP1qzmowao1_400.gif',
         'http://31.media.tumblr.com/1dbbac9bc713e6671a5a299c45538bad/tumblr_mw9f814uIX1szim6vo1_400.jpg',
+        'http://31.media.tumblr.com/91ecab6882e84ffa616eef2d40432bcb/tumblr_ms90rrgzwt1szim6vo1_400.jpg',
         'http://31.media.tumblr.com/bafd9d6c3902ca21c8d4e7a3e75e1253/tumblr_mr696b9BGu1szim6vo1_400.jpg',
         'http://hugelolcdn.com/i/199024.gif',
         'http://i.imgur.com/15q15aa.jpg',
@@ -39,19 +42,15 @@ module.exports = {
         'http://i0.kym-cdn.com/photos/images/newsfeed/000/581/296/c09.jpg',
         'http://i1.kym-cdn.com/photos/images/newsfeed/000/581/567/bab.jpg',
         'http://i2.kym-cdn.com/photos/images/newsfeed/000/581/168/9f1.jpg',
+        'http://i2.minus.com/ilXWkyP7QN3EX.jpg',
         'http://i3.kym-cdn.com/photos/images/original/000/582/646/ed6.jpg',
+        'http://static.fjcdn.com/comments/Have+another+shibe+_a99207a3a6813dde5ade843ac0786f2c.jpg',
+        'http://static.fjcdn.com/pictures/Shibe_ca1bd3_4825797.jpg',
         'http://static1.fjcdn.com/comments/4707626+_36cc6d9778b7e83fcd4c780dab841945.png',
         'http://static1.fjcdn.com/comments/How+did+this+even+start+_7155a69fce359eb865d3b0a5c27f1359.jpg',
         'http://static2.fjcdn.com/comments/I+m+gonna+post+a+few+_54013ba869e922f46bff8546ef048759.jpg',
-        'http://twilight.ponychan.net/chan/files/src/138327501031.jpg',
-        'http://i2.minus.com/ilXWkyP7QN3EX.jpg',
-        'http://static.fjcdn.com/comments/Have+another+shibe+_a99207a3a6813dde5ade843ac0786f2c.jpg',
-        'http://static.fjcdn.com/pictures/Shibe_ca1bd3_4825797.jpg',
-        'http://24.media.tumblr.com/cce2287cd5eb22ad85d416f33cdfc4c2/tumblr_mqasxzAD5z1szim6vo1_400.jpg',
-        'http://31.media.tumblr.com/bafd9d6c3902ca21c8d4e7a3e75e1253/tumblr_mr696b9BGu1szim6vo1_400.jpg',
         'http://static4.fjcdn.com/comments/+_f6b3059315b636fcbaae98bd5533fb6b.jpg',
-        'http://31.media.tumblr.com/91ecab6882e84ffa616eef2d40432bcb/tumblr_ms90rrgzwt1szim6vo1_400.jpg',
-        'http://2damnfunny.com/wp-content/uploads/2013/10/Shibe-Doge-Declares-Siberia-The-Home-Of-Wow.jpg'
+        'http://twilight.ponychan.net/chan/files/src/138327501031.jpg'
     ]
 
 };
